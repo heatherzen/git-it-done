bb622f8411516150b50cf167dae396f3f4a9c9d7
heatherzen <heather.zen@gmail.com>
2020-07-01 23:11:02 -0700

    halfway thru 6.2.5

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,13 +1,41 @@
-var getUserRepos = function(user) {
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+
+
+
+
+var getUserRepos = function (user) {
     //format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
     //make a request to the url
-    fetch(apiUrl).then(function(response) {
-        response.json().then(function(data) {
-            console.log(data);
+    fetch(apiUrl).then(function (response) {
+        response.json().then(function (data) {
+            displayRepos(data, user);
         });
     });
 };
 
-getUserRepos();
+
+
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+    //get value from input element
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+    console.log(event);
+};
+
+var displayRepos = function(repos, searchTerm) {
+    console.log(repos);
+    console.log(searchTerm);
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
