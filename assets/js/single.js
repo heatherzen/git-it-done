0cba440e90eea5fd7277664218efd239d64a4f5c
heatherzen <heather.zen@gmail.com>
2020-07-03 22:03:40 -0700

    working link to issues and data across all files

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,11 +1,12 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
-
+var repoNameEl = document.querySelector("#repo-name");
 
 var getRepoName = function() {
     var queryString = document.location.search;
     var repoName = queryString.split("=")[1];
-    console.log(repoName);
+    getRepoIssues(repoName);
+    repoNameEl.textContent = repoName;
 }
 
 var getRepoIssues = function(repo) {
