4ed104d77b19ffe9c13f071f5d36fe2395059ebb
heatherzen <heather.zen@gmail.com>
2020-07-03 22:17:43 -0700

    added error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -3,10 +3,18 @@ var limitWarningEl = document.querySelector("#limit-warning");
 var repoNameEl = document.querySelector("#repo-name");
 
 var getRepoName = function() {
+    //grab repo name from url query string
     var queryString = document.location.search;
     var repoName = queryString.split("=")[1];
-    getRepoIssues(repoName);
-    repoNameEl.textContent = repoName;
+
+    if (repoName) {
+        //display repo name on the page
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    } else { 
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
 }
 
 var getRepoIssues = function(repo) {
@@ -26,7 +34,8 @@ var getRepoIssues = function(repo) {
             });
         }
         else {
-            alert("There was a problem with your request!");
+            //if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
     });
 };
